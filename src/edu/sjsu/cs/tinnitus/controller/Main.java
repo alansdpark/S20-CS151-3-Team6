57042e52bb6f93febcdf07f2e09fb5bd4106b194
Alan Park <alansdpark@gmail.com>
2020-04-14 16:08:19 -0700

    Create Main.java

---
+++ src/edu/sjsu/cs/tinnitus/controller/Main.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class Main {
+    /**
+     * Run project.
+     */
+    public static void main(String[] args)
+    {
+        JFrame window = new JFrame("Team 6 - Project 3");
+    }
+}
