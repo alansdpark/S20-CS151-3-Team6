8b28f53dbca027702d9021876262cc14ae5d6812
Alan Park <alansdpark@gmail.com>
2020-04-14 06:11:59 -0700

    Update InitialInterview.java

--- src/edu/sjsu/cs/tinnitus/model/InitialInterview.java
+++ src/edu/sjsu/cs/tinnitus/model/InitialInterview.java
@@ -2,6 +2,7 @@
 /**
  * Tinnitus/Hyperacusis Initial Interview Form
  * After registering patient, initial interview takes place.
+ * This is to gauge, diagnose, and quantify the patient's hearing problem. 
  */
 public class InitialInterview {
     Audiology audiology;
@@ -11,6 +12,14 @@ public class InitialInterview {
     {
         audiology = new Audiology(); // Perform initial audiology.
         // TODO - Set medical history here
+    }
+
+    /**
+     * Based on audiology and medical history.
+     */
+    public void diagnose()
+    {
+
     }
 
 }
