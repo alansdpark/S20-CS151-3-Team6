a1c1eb64a33d09ebd732309f9bab2bf7c70a5552
Kyle Bascomb <kylebascomb@gmail.com>
2020-04-13 23:16:19 -0700

    Added documentation for fields in medicalHistory.java

--- src/edu/sjsu/cs/tinnitus/model/MedicalHistory.java
+++ src/edu/sjsu/cs/tinnitus/model/MedicalHistory.java
@@ -1,4 +1,54 @@
 package edu.sjsu.cs.tinnitus.model;
 
+import java.util.ArrayList;
+
 public class MedicalHistory {
+
+    public MedicalHistory(String medicalHistoryDescription, ArrayList<Medication> medicationList) {
+        this.medicalHistoryDescription = medicalHistoryDescription;
+        this.medicationList = medicationList;
+    }
+
+    /**
+     * getter for medicalHistoryDescription
+     * @return - medicalHistoryDescription
+     */
+    public String getMedicalHistoryDescription() {
+        return medicalHistoryDescription;
+    }
+
+    /**
+     * setter for medicalHistoryDescription
+     * @param medicalHistoryDescription - medicalHistoryDescription
+     */
+    public void setMedicalHistoryDescription(String medicalHistoryDescription) {
+        this.medicalHistoryDescription = medicalHistoryDescription;
+    }
+
+    /**
+     * getter for medication list
+     * @return - medicationList
+     */
+    public ArrayList<Medication> getMedicationList() {
+        return medicationList;
+    }
+
+    /**
+     * setter for medicationList
+     * @param medicationList - medicationList
+     */
+    public void setMedicationList(ArrayList<Medication> medicationList) {
+        this.medicationList = medicationList;
+    }
+
+    /**
+     * Adds a new medication to the medication list
+     * @param medication - medication to be added
+     */
+    public void addMedication(Medication medication) {
+        medicationList.add(medication);
+    }
+
+    private String medicalHistoryDescription;
+    private ArrayList<Medication> medicationList;
 }
