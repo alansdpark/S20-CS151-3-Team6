bbc3542628a38de91cdaffbceb3b51924f687bf0
Alan Park <alansdpark@gmail.com>
2020-04-14 06:03:06 -0700

    Created new Initial Interview class

--- src/edu/sjsu/cs/tinnitus/model/Audiology.java
+++ src/edu/sjsu/cs/tinnitus/model/Audiology.java
@@ -2,4 +2,5 @@ package edu.sjsu.cs.tinnitus.model;
 
 //TODO
 public class Audiology {
+    
 }
---
+++ src/edu/sjsu/cs/tinnitus/model/InitialInterview.java
@@ -0,0 +1,13 @@
+
+/**
+ * Tinnitus/Hyperacusis Initial Interview Form
+ * After registering patient, initial interview takes place.
+ */
+public class InitialInterview {
+    Audiology audiology = new Audiology();
+
+    public InitialInterview()
+    {
+        
+    }
+}
