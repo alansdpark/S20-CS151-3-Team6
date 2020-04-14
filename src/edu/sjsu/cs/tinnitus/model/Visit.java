46c8c48c94102fd67dae8a1a86ae553d0f47887e
Kyle Bascomb <kylebascomb@gmail.com>
2020-04-13 23:32:15 -0700

    Added documentation for fields in Visit.java

--- src/edu/sjsu/cs/tinnitus/model/Visit.java
+++ src/edu/sjsu/cs/tinnitus/model/Visit.java
@@ -1,4 +1,94 @@
 package edu.sjsu.cs.tinnitus.model;
 
 public class Visit {
+
+    /**
+     * Default Constructor for visit
+     */
+    public Visit(){
+
+    }
+
+    /**
+     * Constructor for Visit
+     * @param audiology - audiology associated with the visist
+     * @param patient - patient
+     * @param date - date of the visit
+     * @param visitNumber - visit number recorded in ascending order
+     */
+    public Visit(Audiology audiology, Patient patient, String date, int visitNumber) {
+        this.audiology = audiology;
+        this.patient = patient;
+        this.date = date;
+        this.visitNumber = visitNumber;
+    }
+
+    /**
+     * getter for audiology
+     * @return - audiology
+     */
+    public Audiology getAudiology() {
+        return audiology;
+    }
+
+    /**
+     * setter for audiology
+     * @param audiology - audiology
+     */
+    public void setAudiology(Audiology audiology) {
+        this.audiology = audiology;
+    }
+
+    /**
+     * getter for patient
+     * @return - patient
+     */
+    public Patient getPatient() {
+        return patient;
+    }
+
+    /**
+     * setter for patient
+     * @param patient - patient
+     */
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    /**
+     * getter for date
+     * @return - date
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * setter for date
+     * @param date - date
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * getter for visitNumber
+     * @return - visitNumber
+     */
+    public int getVisitNumber() {
+        return visitNumber;
+    }
+
+    /**
+     * setter for visitNumber
+     * @param visitNumber
+     */
+    public void setVisitNumber(int visitNumber) {
+        this.visitNumber = visitNumber;
+    }
+
+    private Audiology audiology;
+    private Patient patient;
+    private String date;
+    private int visitNumber;
 }
