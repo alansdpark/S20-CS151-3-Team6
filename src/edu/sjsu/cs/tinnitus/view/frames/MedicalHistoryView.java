ec79519c562d6bfffd267ab8a00dab114bce9ace
Alan Park <alansdpark@gmail.com>
2020-04-15 12:44:27 -0700

    Update MedicalHistoryView.java
    Added constructors

--- src/edu/sjsu/cs/tinnitus/view/frames/MedicalHistoryView.java
+++ src/edu/sjsu/cs/tinnitus/view/frames/MedicalHistoryView.java
@@ -12,8 +12,22 @@ public class MedicalHistoryView extends JPanel
     private JButton editMedicalHistory;
     private JButton editMedication;
 
-    public MedicalHistoryView()
-    {
+    /**
+     * Default Constructor
+     */
+    public MedicalHistoryView() {
+        medicalHistory = new JLabel();
+        currentMedication = new JLabel();
+    }
 
+    /**
+     * Constructor with medical history and current medication already created.
+     * @param medHistory
+     * @param currentMed
+     */
+    public MedicalHistoryView(String medHistory, String currentMed)
+    {
+        medicalHistory = new JLabel(medHistory);
+        currentMedication = new JLabel(currentMed);
     }
 }
