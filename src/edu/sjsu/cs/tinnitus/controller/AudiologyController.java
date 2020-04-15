92e6e1834b47be92b4cf6831a3b3beea6e96ae86
Alan Park <alansdpark@gmail.com>
2020-04-14 20:23:57 -0700

    Update AudiologyController.java

--- src/edu/sjsu/cs/tinnitus/controller/AudiologyController.java
+++ src/edu/sjsu/cs/tinnitus/controller/AudiologyController.java
@@ -1,5 +1,8 @@
 package controller;
 
+/**
+ * Controller for Audiology.
+ */
 public class AudiologyController 
 {
     /**
@@ -13,6 +16,9 @@ public class AudiologyController
      */
     private int stage; 
 
+    /**
+     * New Controller should be created for every test.
+     */
     public AudiologyController()
     {
         stage = 1;
@@ -32,16 +38,22 @@ public class AudiologyController
     public void performTest(int stage)
     {
         switch (stage) {
+            // Pure-Tone audiogram
             case 1:
                 break;
+            // LDL (Loudness Discomfort Levels)
             case 2:
                 break;
+            // Tinnitus pitch match/type
             case 3:
                 break;
+            // Thresholds of hearing
             case 4:
                 break;
+            // Minimal masking levels for left and right ear.
             case 5:
                 break;
+            // DEFAULT - Should not be here.
             default:
                 System.out.println("performTest() malfunctioned.");
         }
