20dd61d1104a3aa07317e65e9a586bafff2cf81f
Alan Park <alansdpark@gmail.com>
2020-04-15 12:41:42 -0700

    Fixed documentation

--- src/edu/sjsu/cs/tinnitus/view/frames/MedicalHistoryView.java
+++ src/edu/sjsu/cs/tinnitus/view/frames/MedicalHistoryView.java
@@ -1,8 +1,17 @@
 package edu.sjsu.cs.tinnitus.view.frames;
 import javax.swing.*;
 
+/**
+ * View for Medical History Page.
+ */
 public class MedicalHistoryView extends JPanel
 {
+    private JLabel medicalHistory;
+    private JLabel currentMedication;
+    // Edit buttons
+    private JButton editMedicalHistory;
+    private JButton editMedication;
+
     public MedicalHistoryView()
     {
 
