f850f7123c25596607acf1c6c363eadd2f9567d9
Alan Park <alansdpark@gmail.com>
2020-04-15 00:20:40 -0700

    Replaced system.java with PatientTable.java

--- src/edu/sjsu/cs/tinnitus/model/PatientTable.java
+++ src/edu/sjsu/cs/tinnitus/model/PatientTable.java
@@ -10,5 +10,34 @@ public class PatientTable
         patients = new ArrayList<>();
     }
 
+    /**
+     * Constructor for the System
+     * @param patientList - list of patients
+     */
+    public PatientTable(ArrayList<Patient> patientList) {
+        this.patients = patientList;
+    }
 
+    /**
+     * Getter for patientList
+     * @return - patientList
+     */
+    public ArrayList<Patient> getPatientList() {
+        return patients;
+    }
+
+    /**
+     * Setter for patientList
+     * @param patientList - patientList
+     */
+    public void setPatientList(ArrayList<Patient> patientList) {
+        this.patients = patientList;
+    }
+
+    /**
+     * Adds a patient to the patientList
+     */
+    public void addPatient(Patient patient){
+        patients.add(patient);
+    }
 }
