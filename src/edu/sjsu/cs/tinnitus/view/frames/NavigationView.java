0eb124638d4cd161f0c030f8a5e2bca1c54848b5
Alan Park <alansdpark@gmail.com>
2020-04-14 17:27:52 -0700

    navigation view additions

--- src/edu/sjsu/cs/tinnitus/view/frames/NavigationView.java
+++ src/edu/sjsu/cs/tinnitus/view/frames/NavigationView.java
@@ -2,8 +2,23 @@ package view.frames;
 import javax.swing.*;
 
 public class NavigationView extends JPanel {
+
+    /**
+     * Navigation Buttons
+     */
+    private JButton registerNewPatient;
+    private JButton viewPatients;
+    private JButton logVisit;
+    private JButton exit;
+
+    /**
+     * 
+     */
     public NavigationView()
     {
-
+        registerNewPatient = new JButton("Register a new patient");
+        viewPatients = new JButton("View all patients");
+        logVisit = new JButton("Log a new visit");
+        exit = new JButton("Exit");
     }
 }
