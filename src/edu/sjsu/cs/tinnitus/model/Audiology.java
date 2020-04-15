4a03b0f131435ee6d0f2ffdbbba6d22a318d078b
Alan Park <alansdpark@gmail.com>
2020-04-14 17:35:22 -0700

    Update Audiology.java

--- src/edu/sjsu/cs/tinnitus/model/Audiology.java
+++ src/edu/sjsu/cs/tinnitus/model/Audiology.java
@@ -3,5 +3,45 @@ package edu.sjsu.cs.tinnitus.model;
 // Perform audiological evaluation and register medical history including pharmacology.
 public class Audiology 
 {
+    private ArrayList<String> comments;
+
+    public Audiology()
+    {
+        comments = new ArrayList<>();
+    }
     
+    public void addComment(String comment)
+    {
+        comments.add(comment);
+    }
+
+    public ArrayList<String> getComments()
+    {
+        return comments;
+    }
+
+    public void puretoneAudiogramTest()
+    {
+
+    }
+
+    public void loudnessDiscomfortTest()
+    {
+
+    }
+
+    public void tinnitusPitchMatchTest()
+    {
+
+    }
+
+    public void thresholdHearingTest()
+    {
+
+    }
+
+    public void minimalMaskingLevelTest()
+    {
+
+    }
 }
