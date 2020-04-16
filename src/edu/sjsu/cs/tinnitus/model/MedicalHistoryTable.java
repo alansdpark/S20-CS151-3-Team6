7c1689f6efe20c38d041bbbaaea9da2853e2a9bd
Alan Park <alansdpark@gmail.com>
2020-04-15 20:54:25 -0700

    Added medicalhistorytable.java

--- src/edu/sjsu/cs/tinnitus/model/MedicalHistoryTable.java
+++ src/edu/sjsu/cs/tinnitus/model/MedicalHistoryTable.java
@@ -6,10 +6,72 @@ package model;
  */
 public class MedicalHistoryTable 
 {
+
+    private ArrayList<Medication> medicationList;
+    private String medicalHistoryDescription;
+    /**
+     * Default constructor.
+     */
     public MedicalHistoryTable()
     {
 
     }
 
-    
+    /**
+     * Constructor with medication list and medical history.
+     * @param medList
+     * @param medHistoryDesc
+     */
+    public MedicalHistoryTable(ArrayList<Medication> medList, String medHistoryDesc)
+    {
+        this.medicationList = medList;
+        this.medicalHistoryDescription = medHistoryDesc;
+    }
+
+    /**
+     * Sets medication list to parameter.
+     * @param medList medication arraylist
+     */
+    public void setMedicationList(ArrayList<Medication> medList)
+    {
+        this.medicationList = medList;
+    }
+
+    /**
+     * Adds to medication list.
+     * @param med medicine
+     */
+    public void addToMedicationList(Medication med)
+    {
+        this.medicationList.add(med);
+    }
+
+    /**
+     * Returns medication list.
+     * @return medication arraylist
+     */
+    public ArrayList<Medication> getMedicationList()
+    {
+        return this.medicationList;
+    }
+
+    /**
+     * Sets medical history description.
+     * @param desc String medical history description.
+     */
+    public void setMedHistoryDescription(String desc)
+    {
+        this.medicalHistoryDescription = desc;
+    }
+
+    /**
+     * Returns medical history description.
+     * @return String of medical history description.
+     */
+    public String getMedHistoryDescription()
+    {
+        return this.medicalHistoryDescription;
+    }
+
+
 }
