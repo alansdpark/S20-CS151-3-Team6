e288ca8035c0d1658b3585288c1278157bf87f48
Alan Park <alansdpark@gmail.com>
2020-04-15 20:12:13 -0700

    Create MedicalHistoryTable.java

---
+++ src/edu/sjsu/cs/tinnitus/model/MedicalHistoryTable.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Similar to PatientTable.java
+ * Acts as object to hold medical history.
+ */
+public class MedicalHistoryTable 
+{
+    public MedicalHistoryTable()
+    {
+
+    }
+
+    
+}
