eb1754a0388554d52378f8f27f6e41e7b638ad5d
miguelv-855 <63612483+MiguelV-855@users.noreply.github.com>
2020-05-14 12:45:11 -0700

    Branch may be missing work, loking into it

--- src/edu/sjsu/cs/tinnitus/view/frames/AudiologyView.java
+++ src/edu/sjsu/cs/tinnitus/view/frames/AudiologyView.java
@@ -137,214 +137,390 @@ public class AudiologyView extends JPanel
         centerPanel.add(hz_12000_field_ldl_right);
     }
 
+    /**
+     * Getter for frequency
+     * @return
+     */
     public JLabel getFrequency() {
         return frequency;
     }
 
+    /**
+     * Getter for TextArea AdditionComments
+     * @return additionalComments
+     */
     public JTextArea getAdditionalCommnents() {
-        return additionalCommnents;
+        return additionalComments;
     }
+
+    /**
+     * Getter for Save JButton
+     * @return saveButton
+     */
     public JButton getSaveButton() {
         return saveButton;
     }
+
+    /**
+     * Getter for Cancel JButton
+     * @return cancelButton
+     */
     public JButton getCancelButton() {
         return cancelButton;
     }
-
+    /**
+     *Getter for LdlLeft Jlabel
+     * @return ldlLeft
+     */
     public JLabel getLdlLeft() {
         return ldlLeft;
     }
+
+    /**
+     * Getter for LdlRight JLabel
+     * @return ldlRight
+     */
     public JLabel getLdlRight() {
         return ldlRight;
     }
 
+    /**
+     * Getter for CenterPanel JPanel
+     * @return centerPanel
+     */
     public JPanel getCenterPanel() {
         return centerPanel;
     }
-
+    /**
+     * Getter for MatchType  JLabel
+     * @return matchType
+     */
     public JLabel getMatchType() {
         return matchType;
     }
 
+    /**
+     * Getter for minMaskLeft JLabel
+     * @return minMaskLeft
+     */
     public JLabel getMinMaskLeft() {
         return minMaskLeft;
     }
-
+    /**
+     * Getter for minMaskRight JLabel
+     * @return minMaskRight
+     */
     public JLabel getMinMaskRight() {
         return minMaskRight;
     }
 
+    /**
+     * Getter for pitchMatch JLabel
+     * @return pitchMatch
+     */
     public JLabel getPitchMatch() {
         return pitchMatch;
     }
-
+    /**
+     * Getter for pureToneLeft JLabel
+     * @return pureToneLeft
+     */
     public JLabel getPureToneLeft() {
         return pureToneLeft;
     }
-
+    /**
+     * Getter for pureToneRight JLabel
+     * @return pureToneRight
+     */
     public JLabel getPureToneRight() {
         return pureToneRight;
     }
 
+    /**
+     * Getter for thresholdLeft JLabel
+     * @return thresholdLeft
+     */
     public JLabel getThresholdLeft() {
         return thresholdLeft;
     }
-
+    /**
+     * Getter for thresholdRight JLabel
+     * @return thresholdRight
+     */
     public JLabel getThresholdRight() {
         return thresholdRight;
     }
-
+    /**
+     * Getter for title JLabel
+     * @return title
+     */
     public JLabel getTitle() {
         return title;
     }
-
+    /**
+     * Getter for hzPanel JPanel
+     * @return hzPanel
+     */
     public JPanel getHzPanel() {
         return hzPanel;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getHz_250_field_pureTone_left() {
         return hz_250_field_pureTone_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_500_field_pureTone_left() {
         return hz_500_field_pureTone_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_1000_field_pureTone_left() {
         return hz_1000_field_pureTone_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_2500_field_pureTone_left() {
         return hz_2500_field_pureTone_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_5000_field_pureTone_left() {
         return hz_5000_field_pureTone_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_7500_field_pureTone_left() {
         return hz_7500_field_pureTone_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_10000_field_pureTone_left() {
         return hz_10000_field_pureTone_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_12000_field_pureTone_left() {
         return hz_12000_field_pureTone_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_250_field_pureTone_right() {
         return hz_250_field_pureTone_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_500_field_pureTone_right() {
         return hz_500_field_pureTone_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_1000_field_pureTone_right() {
         return hz_1000_field_pureTone_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_2500_field_pureTone_right() {
         return hz_2500_field_pureTone_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_5000_field_pureTone_right() {
         return hz_5000_field_pureTone_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_7500_field_pureTone_right() {
         return hz_7500_field_pureTone_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_10000_field_pureTone_right() {
         return hz_10000_field_pureTone_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_12000_field_pureTone_right() {
         return hz_12000_field_pureTone_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_500_field_ldl_left() {
         return hz_500_field_ldl_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_1000_field_ldl_left() {
         return hz_1000_field_ldl_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_2500_field_ldl_left() {
         return hz_2500_field_ldl_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_5000_field_ldl_left() {
         return hz_5000_field_ldl_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_7500_field_ldl_left() {
         return hz_7500_field_ldl_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_10000_field_ldl_left() {
         return hz_10000_field_ldl_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_12000_field_ldl_left() {
         return hz_12000_field_ldl_left.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_500_field_ldl_right() {
         return hz_500_field_ldl_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_1000_field_ldl_right() {
         return hz_1000_field_ldl_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_2500_field_ldl_right() {
         return hz_2500_field_ldl_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_5000_field_ldl_right() {
         return hz_5000_field_ldl_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_7500_field_ldl_right() {
         return hz_7500_field_ldl_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_10000_field_ldl_right() {
         return hz_10000_field_ldl_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getHz_12000_field_ldl_right() {
         return hz_12000_field_ldl_right.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getAdditionalCommnents_area() {
         return additionalCommnents_area.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getMatchType_field() {
         return matchType_field.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getMinMaskLeft_field() {
         return minMaskLeft_field.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getMinMaskRight_field() {
         return minMaskRight_field.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getPitchMatch_field() {
         return pitchMatch_field.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getThresholdLeft_field() {
         return thresholdLeft_field.getText();
     }
-
+    /**
+     * Getter for
+     * @return
+     */
     public String getThresholdRight_field() {
         return thresholdRight_field.getText();
     }
